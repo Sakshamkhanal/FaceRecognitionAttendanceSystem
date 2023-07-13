459b6f84d28828a4e132106a686ca73809c0c879
Saksham Khanal <saksham.khanal01@gmail.com>
2023-07-13 19:38:20 +0545

    Another commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2
 import os
 import pickle
+import face_recognition
+import numpy as np
+import cvzone
 cap = cv2.VideoCapture(0)
 cap.set(3,640)
 cap.set(4,480)
@@ -14,16 +17,42 @@ for path in modePathList:
     imgModeList.append(cv2.imread(os.path.join(folderModePath,path)))
 #print(len(imgModeList))
 #Load the encoding file
+print('Loading Encode File...')
 file = open('EncodeFile.p','rb')
 encodeListKnownWithIds = pickle.load(file)
 file.close()
-encodeListKnown, StudentIds = encodeListKnownWithIds
+encodeListKnown, StudentIds = encodeListKnownWithIds 
 print(StudentIds)
+print('EcodeFile Loaded...')
+
+
 while True:
     success, img = cap.read()
 
+
+    imgS =cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS =cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS,faceCurFrame)
+
     imgBackground[162:162+480,55:55+640] = img #Length and height of the camera images
-    imgBackground[44:44+633,808:808+414] = imgModeList[2]
+    imgBackground[44:44+633,808:808+414] = imgModeList[3]
     #cv2.imshow("Webcam",img)
+
+    for encodeFace,faceLoc in zip(encodeCurFrame,faceCurFrame):
+        matches =face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)  
+        print("matches",matches)  
+        print("faceDis",faceDis) 
+
+        matchIndex = np.argmin(faceDis)
+    #    print("matchIndex",matchIndex) 
+
+        if matches[matchIndex]:
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            bbox = 55 + x1, 162 + y1, x2 - x1, y2 - y1
+            imgBackground = cvzone.cornerRect(imgBackground,bbox,rt=0)
     cv2.imshow("Face Attendance",imgBackground)
     cv2.waitKey(1)
